7846d624f686ba8f35fcf09b9efe8810bf95cede
Jeremy339 <jal.pacho@yavirac.edu.ec>
2024-06-02 18:18:34 -0500

    Igualado pero con Errores

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,10 +5,14 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
 
 
 @NgModule({
+  providers:[
+    AuthService
+  ],
   declarations: [
     LoginComponent,
     RegisterComponent
