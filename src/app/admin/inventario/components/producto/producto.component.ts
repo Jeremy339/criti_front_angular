3ce2220a74952dd84333beed4d2dfb7c12ceb7f9
Jeremy339 <jal.pacho@yavirac.edu.ec>
2024-06-12 19:33:45 -0500

    Igualado a clase 16 y carpetas creadas, menos components

--- src/app/admin/inventario/components/producto/producto.component.ts
+++ src/app/admin/inventario/components/producto/producto.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrl: './producto.component.scss'
 })
 export class ProductoComponent {
+  libros: any[] = [{ id: 1, titulo: "El Ice Cube", autor: "Autor Anónimo", sinopsis: "La historia continua", categoria_id: 1 },
+    { id: 1, titulo: "El Ice Cube", autor: "Autor Anónimo", sinopsis: "La historia continua", categoria_id: 1 },
+    { id: 1, titulo: "El Ice Cube", autor: "Autor Anónimo", sinopsis: "La historia continua", categoria_id: 1 },
+    { id: 1, titulo: "El Ice Cube", autor: "Autor Anónimo", sinopsis: "La historia continua", categoria_id: 1 },
+    { id: 1, titulo: "El Ice Cube", autor: "Autor Anónimo", sinopsis: "La historia continua", categoria_id: 1 },
+    { id: 1, titulo: "El Ice Cube", autor: "Autor Anónimo", sinopsis: "La historia continua", categoria_id: 1 },
+    { id: 1, titulo: "El Ice Cube", autor: "Autor Anónimo", sinopsis: "La historia continua", categoria_id: 1 },
+    { id: 1, titulo: "El Ice Cube", autor: "Autor Anónimo", sinopsis: "La historia continua", categoria_id: 1 },
+    { id: 1, titulo: "El Ice Cube", autor: "Autor Anónimo", sinopsis: "La historia continua", categoria_id: 1 },
+    { id: 1, titulo: "El Ice Cube", autor: "Autor Anónimo", sinopsis: "La historia continua", categoria_id: 1 },
+    { id: 1, titulo: "El Ice Cube", autor: "Autor Anónimo", sinopsis: "La historia continua", categoria_id: 1 },
+    { id: 1, titulo: "El Ice Cube", autor: "Autor Anónimo", sinopsis: "La historia continua", categoria_id: 1 },
+    { id: 1, titulo: "El Ice Cube", autor: "Autor Anónimo", sinopsis: "La historia continua", categoria_id: 1 },
+    { id: 1, titulo: "El Ice Cube", autor: "Autor Anónimo", sinopsis: "La historia continua", categoria_id: 1 },
+    { id: 1, titulo: "El Ice Cube", autor: "Autor Anónimo", sinopsis: "La historia continua", categoria_id: 1 },
+ 
+  ];
+  cols: any[] = []
 
+  openNew() {
+
+  }
+  editMateria(mate: any) {
+
+  }
+  deleteMateria(mate: any) {
+
+  }
 }
